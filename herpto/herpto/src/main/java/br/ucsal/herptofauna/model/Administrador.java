3ebd6c4fe05c336df5a00233923afd6f4cdcc6f7
Jamille <mille.bittencourt@gmail.com>
2018-11-17 20:39:28 -0300

    Revert "Primeiro Commit - configurações e Usuario"
    This reverts commit dda561907ff1904fbd314a47d72bdc04731d0544.

--- herpto/herpto/src/main/java/br/ucsal/herptofauna/model/Administrador.java
+++
@@ -1,9 +0,0 @@
-package br.ucsal.herptofauna.model;
-
-public class Administrador extends Usuario{
-
-	public Administrador(int id, String nome, String login, String senha) {
-		super(id, nome, login, senha);	
-	}
-
-}

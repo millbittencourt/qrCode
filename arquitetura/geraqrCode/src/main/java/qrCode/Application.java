f04d071026fa6c0dd83d0feafb9cad42a7421cef
Jamille <mille.bittencourt@gmail.com>
2018-11-17 20:32:57 -0300

    Commmit

--- arquitetura/geraqrCode/src/main/java/qrCode/Application.java
+++ arquitetura/geraqrCode/src/main/java/qrCode/Application.java
@@ -1,4 +1,4 @@
-package qrCoe;
+package qrCode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- arquitetura/geraqrCode/src/main/java/qrCode/QrCodeController.java
+++ arquitetura/geraqrCode/src/main/java/qrCode/QrCodeController.java
@@ -1,4 +1,4 @@
-package qrCoe;
+package qrCode;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
